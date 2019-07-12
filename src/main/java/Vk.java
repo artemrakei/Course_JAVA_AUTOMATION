c99eba923065ca539091adc7af17c088dafb668f
artemrakei <50516037+artemrakei@users.noreply.github.com>
2019-07-12 14:47:03 +0300

    Занятие 14

--- src/main/java/Vk.java
+++ src/main/java/Vk.java
@@ -8,47 +8,73 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+
 public class Vk {
     private URIBuilder builder;
-    public String post_id;
+   public String post_id;
+
+
 
     //+375292538629
     // ct6aLKjayVXZstU
+//    public static void main( String[] args ) throws IOException, URISyntaxException
+//    {
+//        HttpClient client = HttpClientBuilder.create().build();
+//        URIBuilder builder = new URIBuilder("https://api.vk.com/method/wall.post?");
+//        builder.setParameter("access_token", "a514cb82bf1bad069a6df42e7289dfb9e8f584bfeb78ba7c22515785ce09e8b75333bfc8a59ad896a4e8b")
+//                .setParameter("owner_id", "552470481")
+//                .setParameter("message", "Hello")
+//                .setParameter("v", "5.101");
+//        HttpGet request = new HttpGet(builder.build());
+//        HttpResponse response = client.execute(request);
+//        System.out.println(EntityUtils.toString(response.getEntity()));
+//    }
 
+
+    public HttpResponse execute(URIBuilder builder) throws URISyntaxException, IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(builder.build());
+        HttpResponse response = client.execute(request);
+        String result = EntityUtils.toString(response.getEntity());
+        post_id = result.replaceAll("[^0-9]", "");
+        System.out.println(post_id);
+
+        return client.execute(request);
+    }
     public URIBuilder createBasicURL(String methodName) throws URISyntaxException {
+
         String urlName = String.format("https://api.vk.com/method/wall.%s?", methodName);
         builder = new URIBuilder(urlName);
-        builder.setParameter("accaes_token", "ab8635bef89a032945d71e4d69c0ba9d01dda78907b3714ec860dc518ea352d25c0c5350a46036f5167da")
-                .setParameter("owner_id", "552470481")
+        builder.setParameter("owner_id", "552470481")
+                .setParameter("access_token", "d8752845fb8e3859cdc1e01736cc51bed955b6c6ed4f2bbee347e6970e5b7e29e7a35a19383feded33ea8")
                 .setParameter("v", "5.101");
         return builder;
     }
 
+
     public URIBuilder postMessages(String message) throws URISyntaxException {
-        URIBuilder builder = createBasicURL("post");
+        builder = createBasicURL("post");
+
         builder.setParameter("message", message);
+
         return builder;
     }
 
-    public URIBuilder editMessages(String message) throws URISyntaxException {
-        URIBuilder builder = createBasicURL("edit");
-        builder.setParameter("post_id", post_id);
+    public URIBuilder editMessages(String message, String param) throws URISyntaxException {
+
+        builder = createBasicURL("edit");
+        builder.setParameter("post_id", param);
         builder.setParameter("message", message);
+
         return builder;
     }
 
-    public URIBuilder deleteMessages() throws URISyntaxException {
-        URIBuilder builder = createBasicURL("delete");
-        builder.setParameter("post_id", post_id);
+    public URIBuilder deleteMessages(String param) throws URISyntaxException {
+        builder = createBasicURL("delete");
+        builder.setParameter("post_id", param);
+
         return builder;
     }
 
-    public HttpResponse execute(URIBuilder builder) throws URISyntaxException, IOException {
-        HttpGet request = new HttpGet(builder.build());
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpResponse response = client.execute(request);
-        String result = EntityUtils.toString(response.getEntity());
-        post_id = result.substring(23, 26);
-        return client.execute(request);
-    }
+
 }
--- src/test/java/VkTest.java
+++ src/test/java/VkTest.java
@@ -1,35 +1,53 @@
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.util.EntityUtils;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class VkTest {
+    private static final Logger log = Logger.getLogger(Vk.class);
     Vk vk;
+    private static String post_id;
 
-    @BeforeEach
+
+
+
+@BeforeTest
     void setUp() {
         vk = new Vk();
     }
 
-    @Test
+
+    @Test (priority = 1)
     public void postMessageTest() throws URISyntaxException, IOException {
+        log.info("It's information message");
         URIBuilder builder = vk.postMessages("Hello");
         EntityUtils.toString(vk.execute(builder).getEntity());
+        post_id=vk.post_id;
+        System.out.println(post_id);
     }
-
-    @Test
+    @Test (priority = 2)
     public void editMessageTest() throws URISyntaxException, IOException {
-        URIBuilder builder = vk.editMessages("Hello World");
+        log.warn("Maybe this will not work");
+        System.out.println(post_id);
+        URIBuilder builder = vk.editMessages("Hello World",post_id);
         EntityUtils.toString(vk.execute(builder).getEntity());
+
+    }
+    @Test (priority = 3)
+    public void deleteMessageTest() throws URISyntaxException, IOException {
+        log.error("But you should try running it");
+        log.fatal("As you see nothing happened");
+        System.out.println(post_id);
+        URIBuilder builder = vk.deleteMessages(post_id);
+        EntityUtils.toString(vk.execute(builder).getEntity());
+
     }
 
-    @Test
-    public void deleteMessageTest() throws URISyntaxException, IOException {
-        URIBuilder builder = vk.deleteMessages();
-        EntityUtils.toString(vk.execute(builder).getEntity());
-    }
+
+
 
 }
